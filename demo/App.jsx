b87dd90dbc17720ae2aa65372e1b4725b8f95b24
Bryant Williams <bryant@sharethis.com>
2018-09-11 09:48:25 -0700

    add reaction buttons and follow buttons

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,42 +1,133 @@
 import React from "react";
-import {InlineShareButtons, StickyShareButtons} from 'sharethis-reactjs';
+import {InlineReactionButtons} from 'sharethis-reactjs';
+import {InlineShareButtons} from 'sharethis-reactjs';
+import {StickyShareButtons} from 'sharethis-reactjs';
+import {InlineFollowButtons} from 'sharethis-reactjs';
 
 class App extends React.Component {
 
   render () {
     return (
       <div>
-        <InlineShareButtons config={{
-          alignment: 'center',
-          enabled: true,
-          fade_in: true,
-          font_size: 16,
-          labels: 'cta',
-          language: 'en',
-          networks: ['whatsapp', 'linkedin', 'messenger', 'facebook', 'twitter'],
-          padding: 12,
-          radius: 4,
-          show_total: true,
-          size: 40,
-          url: 'https://www.sharethis.com'
-        }} />
-        <StickyShareButtons config={{
-          alignment: 'left',
-          enabled: true,
-          hide_desktop: false,
-          labels: 'counts',
-          langauge: 'en',
-          min_count: 10,
-          networks: ['linkedin', 'facebook', 'twitter', 'pinterest', 'email'],
-          padding: 12,
-          radius: 4,
-          show_total: true,
-          show_mobile: true,
-          show_toggle: true,
-          size: 48,
-          top: 160,
-          url: 'https://www.sharethis.com'
-        }} />
+        <style dangerouslySetInnerHTML={{__html: `
+          html, body {
+            margin: 0;
+            padding: 0;
+            text-align: center;
+          }
+          h1 {
+            font-size: 24px;
+            font-weight: bold;
+          }
+          hr {
+            margin-bottom: 40px;
+            margin-top: 40px;
+            width: 50%;
+          }
+        `}} />
+
+        <h1>Inline Share Buttons</h1>
+        <InlineShareButtons
+          config={{
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            labels: 'cta',        // button labels (cta, counts, null)
+            language: 'en',       // which language to use (see LANGUAGES)
+            networks: [           // which networks to include (see NETWORKS)
+              'whatsapp',
+              'linkedin',
+              'messenger',
+              'facebook',
+              'twitter'
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,
+            size: 40,             // the size of each button (INTEGER)
+            url: 'https://www.sharethis.com' // url (OPTIONAL) (STRING) (defaults to current url)
+          }}
+        />
+        <hr />
+
+        <h1>Inline Reaction Buttons</h1>
+        <InlineReactionButtons
+          config={{
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            enabled: true,        // show/hide buttons (true, false)
+            language: 'en',       // which language to use (see LANGUAGES)
+            min_count: 0,         // hide react counts less than min_count (INTEGER)
+            padding: 12,          // padding within buttons (INTEGER)
+            reactions: [          // which reactions to include (see REACTIONS)
+              'slight_smile',
+              'heart_eyes',
+              'laughing',
+              'astonished',
+              'sob',
+              'rage'
+            ],
+            size: 48,             // the size of each button (INTEGER)
+            spacing: 8,           // the spacing between buttons (INTEGER)
+            url: 'https://www.sharethis.com' // url (OPTIONAL) (STRING) (defaults to current url)
+          }}
+        />
+        <hr />
+
+        <h1>Inline Follow Buttons</h1>
+        <InlineFollowButtons
+          config={{
+            action: 'Follow us:', // call to action (STRING)
+            action_enable: true,  // show/hide call to action (true, false)
+            action_pos: 'bottom', // position of call to action (left, top, right)
+            alignment: 'center',  // alignment of buttons (left, center, right)
+            enabled: true,        // show/hide buttons (true, false)
+            networks: [           // which networks to include (see NETWORKS)
+              'twitter',
+              'facebook',
+              'instagram',
+              'youtube'
+            ],
+            padding: 8,           // padding within buttons (INTEGER)
+            profiles: {           // social profile links for buttons
+              twitter: 'sharethis',
+              facebook: 'sharethis',
+              instagram: 'sharethis',
+              youtube: '/channel/UCbM93niCmdc2RD9RZbLMP6A?view_as=subscriber'
+            },
+            radius: 9,            // the corner radius on each button (INTEGER)
+            size: 32,             // the size of each button (INTEGER)
+            spacing: 8            // the spacing between buttons (INTEGER)
+          }}
+        />
+        <hr />
+
+        <StickyShareButtons
+          config={{
+            alignment: 'left',    // alignment of buttons (left, right)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            hide_desktop: false,  // hide buttons on desktop (true, false)
+            labels: 'counts',     // button labels (cta, counts, null)
+            language: 'en',       // which language to use (see LANGUAGES)
+            min_count: 0,         // hide react counts less than min_count (INTEGER)
+            networks: [           // which networks to include (see NETWORKS)
+              'linkedin',
+              'facebook',
+              'twitter',
+              'pinterest',
+              'email'
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,     // show/hide the total share count (true, false)
+            show_mobile: true,    // show/hide the buttons on mobile (true, false)
+            show_toggle: true,    // show/hide the toggle buttons (true, false)
+            size: 48,             // the size of each button (INTEGER)
+            top: 160,             // offset in pixels from the top of the page
+            url: 'https://www.sharethis.com' // url (OPTIONAL) (STRING) (defaults to current url)
+          }}
+        />
+
       </div>
     );
   }
