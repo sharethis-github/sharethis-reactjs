e2843da8a61e0c4fc8a0d2d45de6ccd0ca482fd7
Hao Xu <hao@sharethis.com>
2020-08-04 12:01:56 -0700

    update noImplicitAny

--- src/index.d.ts
+++ src/index.d.ts
@@ -194,8 +194,8 @@ declare module "sharethis-reactjs" {
     username?: string;
   };
 
-  function InlineShareButtons({ config }: { config: InlineShareButtonsConfig }): void;
-  function InlineFollowButtons({ config }: { config: InlineFollowButtonsConfig }): void;
-  function InlineReactionButtons({ config }: { config: InlineReactionButtonsConfig }): void;
-  function StickyShareButtons({ config }: { config: StickyShareButtonsConfig}): void;
+  function InlineShareButtons({ config }: { config: InlineShareButtonsConfig });
+  function InlineFollowButtons({ config }: { config: InlineFollowButtonsConfig });
+  function InlineReactionButtons({ config }: { config: InlineReactionButtonsConfig });
+  function StickyShareButtons({ config }: { config: StickyShareButtonsConfig});
 }
