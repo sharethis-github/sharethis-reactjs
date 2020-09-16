6056e07db12b10ba51b4b9131a592cf1655d1710
lucasflomuller <lucasflorianomuller@gmail.com>
2020-09-16 10:54:18 -0300

    feat: Add option object to individual network

--- demo/App.jsx
+++ demo/App.jsx
@@ -124,7 +124,7 @@ class App extends React.Component {
             language: 'en',       // which language to use (see LANGUAGES)
             min_count: 0,         // hide react counts less than min_count (INTEGER)
             networks: [           // which networks to include (see SHARING NETWORKS)
-              'linkedin',
+              'linkedin',         // network without options
               'facebook',
               'twitter',
               'pinterest',
@@ -150,6 +150,59 @@ class App extends React.Component {
           }}
         />
 
+
+        <StickyShareButtons
+          config={{
+            alignment: 'right',   // alignment of buttons (left, right)
+            color: 'social',      // set the color of buttons (social, white)
+            enabled: true,        // show/hide buttons (true, false)
+            font_size: 16,        // font size for the buttons
+            hide_desktop: false,  // hide buttons on desktop (true, false)
+            labels: 'counts',     // button labels (cta, counts, null)
+            language: 'en',       // which language to use (see LANGUAGES)
+            min_count: 0,         // hide react counts less than min_count (INTEGER)
+            networks: [           // which networks to include (see SHARING NETWORKS)
+              'linkedin',         // network without options
+              'facebook',
+              ['twitter', {       // network with options
+                icon: {           // options applied to the icon
+                  src: 'https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-4.png',
+                  style: {
+                    objectFit: 'contain'
+                  }
+                },
+                text: {           // options applied to the span of the text
+                  style: {
+                    color: 'black'
+                  }
+                },
+                container: {      // options applied to the span of the container
+                  style: {
+                    backgroundColor: 'white',
+                  }
+                }
+              }],
+              'pinterest',
+              'email'
+            ],
+            padding: 12,          // padding within buttons (INTEGER)
+            radius: 4,            // the corner radius on each button (INTEGER)
+            show_total: true,     // show/hide the total share count (true, false)
+            show_mobile: true,    // show/hide the buttons on mobile (true, false)
+            show_toggle: true,    // show/hide the toggle buttons (true, false)
+            size: 48,             // the size of each button (INTEGER)
+            top: 160,             // offset in pixels from the top of the page
+
+            // OPTIONAL PARAMETERS
+            url: 'https://www.sharethis.com', // (defaults to current url)
+            image: 'https://bit.ly/2CMhCMC',  // (defaults to og:image or twitter:image)
+            description: 'custom text',       // (defaults to og:description or twitter:description)
+            title: 'custom title',            // (defaults to og:title or twitter:title)
+            message: 'custom email text',     // (only for email sharing)
+            subject: 'custom email subject',  // (only for email sharing)
+            username: 'custom twitter handle' // (only for twitter sharing)
+          }}
+        />
       </div>
     );
   }
